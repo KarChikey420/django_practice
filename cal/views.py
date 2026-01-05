280219eb460a7dba8126979f24f3246fdea766a7
KarChikey <kartikeynegi2002@gmail.com>
2026-01-05 16:26:57 +0530

    add

--- cal/views.py
+++ cal/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return render(request=request,template_name="home.html")
+    return render(request,"home.html",{'name':'kiran'})
