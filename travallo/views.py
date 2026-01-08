e84aaa2fefd81559ec87cdedfd5b9c57fb2be630
KarChikey <kartikeynegi2002@gmail.com>
2026-01-08 14:07:00 +0530

    add

--- travallo/views.py
+++ travallo/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from .models import Destination
 
 def index(request):
-    return render(request,"index.html")
+    
+    dest1=Destination()
+    dest1.city_name="Mumbai"
+    dest1.country_name="India"
+    return render(request,"index.html",{'dest1':dest1})
+
